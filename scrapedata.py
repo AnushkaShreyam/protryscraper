1b7511076ab1469a934629ae359d0d05d74d4e94
Anushka Shreyam <anushkashreyam10@gmail.com>
2022-09-13 14:02:53 +0530

    Update scrapedata.py

--- scrapedata.py
+++ scrapedata.py
@@ -3,10 +3,10 @@ import urllib
 from bc4 import Blackcoffer
 import time
 import sys
-# Reading an excel file using Python
+# Reading an excel file using Python..
+import xlwt
 import xlrd
 import xlsxwriter
-import xlwt
 from xlwt import Workbook
 
 # Give the location of the file
